e7c2edd565c5018787e56c10dcc0dde0f24742f8
Ocaylak <ocaylak.de@gmail.com>
2021-12-15 11:32:33 +0100

    upload vnetsubnet

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "myterraformnetwork" {
-  name                = "var.vnetname"
+  name                = var.vnetname
   location            = var.location
   resource_group_name = azurerm_resource_group.myterraformgroup.name
   address_space       = ["10.0.0.0/16"]
